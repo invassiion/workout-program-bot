ab2b50f351cc7fa932f9f363c4134a2b373143ad
invassiion <invassiion@gmail.com>
2024-05-08 13:05:48 +0700

    Добавлена обработка команд. TODO - Создать микросервис parser, который будет парсить расписание с сайта университета.

--- node/src/main/java/org/example/service/impl/MainServiceImpl.java
+++ node/src/main/java/org/example/service/impl/MainServiceImpl.java
@@ -1,6 +1,7 @@
 package org.example.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.example.RawData;
 import org.example.dao.AppUserDAO;
 import org.example.dao.RawDataDAO;
@@ -12,6 +13,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 
+import static org.example.service.enums.ServiceCommands.*;
+
+@Log4j
 @RequiredArgsConstructor
 @Service
 public class MainServiceImpl implements MainService {
@@ -23,18 +27,52 @@ public class MainServiceImpl implements MainService {
     @Override
     public void processTextMessage(Update update) {
         saveRawData(update);
-        var textMessage = update.getMessage();
-        var telegramUser = textMessage.getFrom();
-        var appUser = findOrSaveAppUser(telegramUser);
+        var appUser = findOrSaveAppUser(update);
+        var text = update.getMessage().getText();
+        var output = "";
+        
+        if (CANCEL.equals(text)) {
+            output = cancelProcess(appUser);
+        } else if (START.equals(text) || HELP.equals(text)) {
+            output = processServiceCommand(appUser, text);
+        } else {
+            log.error("Unknown command: "+ text );
+            output = "Неизвестная ошибка! Введите /cancel и попробуйте снова!";
+        }
+        
+        var chatId = update.getMessage().getChatId();
+        sendAnswer(output, chatId);
+    }
 
-        var message = update.getMessage();
-        var sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from Node");
+    private void sendAnswer(String output, Long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(output);
         producerService.producerAnswer(sendMessage);
     }
 
-    private AppUser findOrSaveAppUser(User telegramUser) {
+    private String processServiceCommand(AppUser appUser, String cmd) {
+        if (HELP.equals(cmd)) {
+            return help();
+        } else if (START.equals(cmd)) {
+            return "Приветствую! Чтобы посмотреть список доступных команд введите /help";
+        } else {
+            return "Неизвестная команда! Чтобы посмотреть список доступных команд введите /help";
+        }
+    }
+
+    private String help() {
+        return "Список доступных команд:\n"
+                + "/cancel - отмена выполнения текущей команды;";
+    }
+
+    private String cancelProcess(AppUser appUser) {
+        appUserDAO.save(appUser);
+        return "Команда отменена!";
+    }
+
+    private AppUser findOrSaveAppUser(Update update) {
+        User telegramUser = update.getMessage().getFrom();
         AppUser persistentAppUser = appUserDAO.findUserByTelegramUserId(telegramUser.getId());
         if (persistentAppUser == null) {
             AppUser transientAppUser = AppUser.builder()
